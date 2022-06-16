aa3dc6cd18d474ad14bb9bc164b7c21627c25ba9
Swann Martinez <smartinez@xilam.com>
2022-06-16 16:29:53 +0200

    clean: simplify user script folder mapping

--- pythonFiles/launch.py
+++ pythonFiles/launch.py
@@ -10,51 +10,34 @@ sys.path.append(str(include_dir))
 
 from blender_vscode import communication, installation, operators, ui, load_addons, handle_fatal_error, environment
 
+if __name__ == '__main__':
+    try:
+        # Ensure blender version compatibility
+        if bpy.app.version < (2, 80, 34):
+            handle_fatal_error("Please use a newer version of Blender")
 
-def get_user_scripts_folder_path_mapping():
-    # Addons mapping
-    addons_dir = Path(environment.scripts_folder, "addons")
-    addons_folders = [str(d) for d in addons_dir.glob(
-        "*") if Path(d, "__init__.py").is_file()]
-    path_mappings = [{"src": f, "load": f} for f in addons_folders]
+        # Blender_vscode depenencies installation
+        installation.ensure_packages_are_installed(["debugpy", "flask", "requests"],
+                                                environment.allow_modify_external_python)
 
-    # Module mapping
-    modules_dir = Path(environment.scripts_folder, "modules")
-    path_mappings.extend([{"src": str(f), "load": str(f)}
-                         for f in modules_dir.rglob("*.py")])
+        # Debug folder mapping setup
+        if environment.enable_user_script_folder:
+            print("Debug user script folder enabled.")
+            script_dir = Path(environment.scripts_folder)
+            path_mapping = [{"src": str(script_dir), "load": str(script_dir)}]
+        else:
+            path_mapping = load_addons.setup_addon_links(environment.addons_to_load)
 
-    for mapping in path_mappings:
-        print(f"mapping item:  {mapping}")
+        communication.setup(environment.editor_address, path_mapping)
 
-    return path_mappings
+        # Enabling addons
+        load_addons.load(environment.addons_to_load)
 
+        # Register UI panels and Operators
+        ui.register()
+        operators.register()
 
-try:
-    # Ensure blender version compatibility
-    if bpy.app.version < (2, 80, 34):
-        handle_fatal_error("Please use a newer version of Blender")
-
-    # Blender_vscode depenencies installation
-    installation.ensure_packages_are_installed(["debugpy", "flask", "requests"],
-                                               environment.allow_modify_external_python)
-
-    # Debug folder mapping setup
-    if environment.enable_user_script_folder:
-        print("Debug user script folder enabled.")
-        path_mapping = get_user_scripts_folder_path_mapping()
-    else:
-        path_mapping = load_addons.setup_addon_links(environment.addons_to_load)
-
-    communication.setup(environment.editor_address, path_mapping)
-
-    # Enabling addons
-    load_addons.load(environment.addons_to_load)
-
-    # Register UI panels and Operators
-    ui.register()
-    operators.register()
-
-except Exception as e:
-    if type(e) is not SystemExit:
-        traceback.print_exc()
-        sys.exit()
+    except Exception as e:
+        if type(e) is not SystemExit:
+            traceback.print_exc()
+            sys.exit()
